337e423f096ac54ebcef08a1559bee7c8eefc0cd
Yoshihide Jimbo <yjimbo@gmail.com>
2013-08-09 11:24:36 +0900

    Install apps via bash

--- chef-repo/site-cookbooks/dotfiles/recipes/default.rb
+++ chef-repo/site-cookbooks/dotfiles/recipes/default.rb
@@ -104,28 +104,9 @@ node["gitrepos"].each{|obj|
 # OS X Applications 
 #-------------------------------------------------------
 
-dmg_package "Google Chrome" do
-  dmg_name "googlechrome"
-  source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-  checksum "7daa2dc5c46d9bfb14f1d7ff4b33884325e5e63e694810adc58f14795165c91a"
-  action :install
-end
-
 dmg_package "Google Chrome Canary" do
   dmg_name "googlechromecanary"
   source "https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg"
-  action :install
-end
-
-dmg_package "TotalFinder" do
-  source "http://downloads.binaryage.com/TotalFinder-1.4.18.dmg"
-  action :install
-end
-
-dmg_package "Dropbox" do
-  volumes_dir "Dropbox Installer"
-  source "http://www.dropbox.com/download?plat=mac"
-  checksum "b4ea620ca22b0517b75753283ceb82326aca8bc3c86212fbf725de6446a96a13"
   action :install
 end
 
