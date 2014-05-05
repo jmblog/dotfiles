bb0ca08cf02fd6296aa28865953f5c41a0d7a3d3
Yoshihide Jimbo <yjimbo@gmail.com>
2014-05-06 00:24:20 +0900

    Bye Chef

--- chef-repo/site-cookbooks/dotfiles/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'dotfiles'
-maintainer       'Yoshihide Jimbo'
-maintainer_email 'yjimbo@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures dotfiles'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'homebrew'
-depends          'dmg'
