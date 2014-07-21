1059a8c9cef08abb6ea38def754758c7c2f2e1f1
Yoshihide Jimbo <yjimbo@gmail.com>
2014-07-21 20:56:53 +0900

    Install node only if the boxen is not installed

--- node/setup.sh
+++ node/setup.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 source "${HOME}/.bashrc"
+source ${DOTFILES_DIRECTORY}/lib/utils
 
-# Install stable version's node.js via nodebrew
-if [[ ! "$(type -P nodebrew)" ]]; then
-  curl -L git.io/nodebrew | perl - setup
-  source "${HOME}/.bashrc"
-else
-  nodebrew selfupdate
+
+if ! use_boxen; then
+  # Install stable version's node.js via nodebrew
+  if [[ ! "$(type -P nodebrew)" ]]; then
+    curl -L git.io/nodebrew | perl - setup
+    source "${HOME}/.bashrc"
+  else
+    nodebrew selfupdate
+  fi
+
+  nodebrew install-binary stable
+  nodebrew use stable
 fi
-
-nodebrew install-binary stable
-nodebrew use stable
 
 # Install npm packages
 npm update -g npm
