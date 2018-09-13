cd31f944fe806445e4077df2853efe57afdfe143
Yoshihide Jimbo <yjimbo@gmail.com>
2018-09-13 17:33:33 +0900

    Fix zsh/setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -18,14 +18,14 @@ if [[ "$SHELL" != "$binroot/zsh" ]]; then
   sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
 fi
 
-# Mirror dotfiles
-ln -fs "${DOTFILES_DIRECTORY}/zsh/zshrc" "${HOME}/.zshrc"
-
-# Compile .zshrc
-zcompile ~/.zshrc
-
 # Install `sindresorhus/pure` prompt
 curl https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh > "${HOME}/.dotfiles/zsh/pure/pure.zsh"
 curl https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh > "${HOME}/.dotfiles/zsh/pure/async.zsh"
 ln -s "${HOME}/.dotfiles/zsh/pure/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
 ln -s "${HOME}/.dotfiles/zsh/pure/async.zsh" /usr/local/share/zsh/site-functions/async
+
+# Mirror dotfiles
+ln -fs "${DOTFILES_DIRECTORY}/zsh/zshrc" "${HOME}/.zshrc"
+
+# Compile .zshrc
+zcompile ~/.zshrc
