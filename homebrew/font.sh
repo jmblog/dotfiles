cbc070a22a3e4417a8848b1ffc6224be87818dc3
Yoshihide Jimbo <yjimbo@gmail.com>
2018-09-20 10:05:52 +0900

    Add some fonts

--- homebrew/font.sh
+++ homebrew/font.sh
@@ -1,6 +1,7 @@
 brew tap homebrew/cask-fonts
 brew cask install font-bitter
 brew cask install font-fira-code
+brew cask install font-fira-sans
 brew cask install font-fontawesome
 brew cask install font-inconsolata
 brew cask install font-inter-ui
@@ -8,6 +9,7 @@ brew cask install font-lato
 brew cask install font-libre-franklin
 brew cask install font-montserrat
 brew cask install font-noto-sans-cjk-jp
+brew cask install font-oswald
 brew cask install font-open-sans
 brew cask install font-roboto
 brew cask install font-roboto-condensed
