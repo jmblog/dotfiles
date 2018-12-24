e64e18d40d2995140bcc85276ccb5ae540cbbbf3
Yoshihide Jimbo <yjimbo@gmail.com>
2018-12-24 13:32:11 +0900

    Update brew

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -4,3 +4,7 @@ brew install tree
 brew install wget
 brew install openssl
 brew install jq
+brew install ffmpeg
+brew tap GoogleChrome/simplehttp2server https://github.com/GoogleChrome/simplehttp2server
+brew install simplehttp2server
+
