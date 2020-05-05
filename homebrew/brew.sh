3149149a4f0e7eb1328560242ef1ec213d05065d
Yoshihide Jimbo <yjimbo@gmail.com>
2020-05-05 15:08:58 +0900

    Fix brew.sh

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -5,7 +5,7 @@ brew install git-flow
 brew install jq
 brew install kotlin
 brew install openssl
+brew tap GoogleChrome/simplehttp2server https://github.com/GoogleChrome/simplehttp2server
 brew install simplehttp2server
 brew install tree
 brew install wget
-brew tap GoogleChrome/simplehttp2server https://github.com/GoogleChrome/simplehttp2server
