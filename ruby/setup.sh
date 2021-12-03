ca8fd3a452459e0ffb689bae084ebf0a6db687ef
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-03 23:35:47 +0900

    Update setup.sh

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-brew install rbenv ruby-build
-
-# include rbenv init
-rbenv install 2.5.1
-rbenv rehash
-rbenv global 2.5.1
-eval "$(rbenv init -)"
-
-gem install bundler
-rbenv rehash
+brew install rbenv
+rbenv init
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
