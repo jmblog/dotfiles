65e62fb67e4eca92fa438d77bc6c575dbab7d36e
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-03 23:59:13 +0900

    Update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,21 +2,7 @@
 
 DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 
-brew install zsh zsh-completions zsh-autosuggestions
-
-# Make zsh a default shell
-#    1. Add $binroot/zsh into /etc/shells
-#    2. Run `chsh -s $binroot/zsh`
-
-binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
-if [[ "$(type -P $binroot/zsh)" && "$(cat /etc/shells | grep -q "$binroot/zsh")" ]]; then
-  log_subhead "Adding $binroot/zsh to the list of acceptable shells"
-  echo "$binroot/zsh" | sudo tee -a /etc/shells >/dev/null
-fi
-if [[ "$SHELL" != "$binroot/zsh" ]]; then
-  log_subhead "Making $binroot/zsh a default shell"
-  sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
-fi
+brew install zsh-completions zsh-autosuggestions
 
 # Install `sindresorhus/pure` prompt
 curl https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh > "${HOME}/.dotfiles/zsh/pure/pure.zsh"
