6c50ef42447177ac8d9db25934aed98144ec237d
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-03 23:52:48 +0900

    Update font.sh

--- homebrew/font.sh
+++ homebrew/font.sh
@@ -1,17 +1,15 @@
 brew tap homebrew/cask-fonts
-brew cask install font-bitter
-brew cask install font-fira-code
-brew cask install font-fira-sans
-brew cask install font-fontawesome
-brew cask install font-inconsolata
-#brew cask install font-inter-ui
-brew cask install font-lato
-brew cask install font-libre-franklin
-brew cask install font-montserrat
-brew cask install font-noto-sans-cjk-jp
-brew cask install font-oswald
-brew cask install font-open-sans
-brew cask install font-roboto
-brew cask install font-roboto-condensed
-brew cask install font-roboto-mono
-brew cask install font-source-code-pro
+brew install font-bitter
+brew install font-fira-code
+brew install font-fira-sans
+brew install font-inconsolata
+brew install font-lato
+brew install font-libre-franklin
+brew install font-montserrat
+brew install font-noto-sans-cjk-jp
+brew install font-oswald
+brew install font-open-sans
+brew install font-roboto
+brew install font-roboto-condensed
+brew install font-roboto-mono
+brew install font-source-code-pro
