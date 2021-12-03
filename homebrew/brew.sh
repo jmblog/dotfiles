b6df8ef64cb3d0c11478730ddd0e5a0cf147c3a9
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-03 23:39:13 +0900

    Update brew.sh

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -5,9 +5,5 @@ brew install git-flow
 brew install jq
 brew install kotlin
 brew install openssl
-brew tap GoogleChrome/simplehttp2server https://github.com/GoogleChrome/simplehttp2server
-brew install simplehttp2server
-brew install svn # needs to install some fonts with homebrew
 brew install tree
 brew install wget
-brew install github/gh/gh
