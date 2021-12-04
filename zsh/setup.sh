52f98915904b22f44a87cef710a0e5870cbdb3da
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-04 12:04:22 +0900

    Update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -5,7 +5,7 @@ DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 brew install zsh-completions zsh-autosuggestions
 
 # Install `sindresorhus/pure` prompt
-npm install --global pure-prompt
+brew install pure
 
 # Mirror dotfiles
 ln -fs "${DOTFILES_DIRECTORY}/zsh/zshrc" "${HOME}/.zshrc"
