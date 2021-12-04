845d785f2511e2cbeb66f0be814ed8892083efb3
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-04 11:52:32 +0900

    Update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -5,10 +5,7 @@ DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 brew install zsh-completions zsh-autosuggestions
 
 # Install `sindresorhus/pure` prompt
-curl https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh > "${HOME}/.dotfiles/zsh/pure/pure.zsh"
-curl https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh > "${HOME}/.dotfiles/zsh/pure/async.zsh"
-ln -s "${HOME}/.dotfiles/zsh/pure/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
-ln -s "${HOME}/.dotfiles/zsh/pure/async.zsh" /usr/local/share/zsh/site-functions/async
+npm install --global pure-prompt
 
 # Mirror dotfiles
 ln -fs "${DOTFILES_DIRECTORY}/zsh/zshrc" "${HOME}/.zshrc"
