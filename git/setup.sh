30e946ae6cfcbc0b641a2a1bf8b26e7208ecdc0c
Yoshihide Jimbo <yjimbo@gmail.com>
2021-12-04 19:03:53 +0900

    Fix homebrew path

--- git/setup.sh
+++ git/setup.sh
@@ -4,7 +4,7 @@ DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 
 # Install GitHub CLI
 brew install gh
-gh completion -s zsh > /usr/local/share/zsh/site-functions/_gh
+gh completion -s zsh > $(brew --prefix)/share/zsh/site-functions/_gh
 
 
 ln -fs "${DOTFILES_DIRECTORY}/git/gitattributes" "${HOME}/.gitattributes"
@@ -13,4 +13,4 @@ ln -fs "${DOTFILES_DIRECTORY}/git/gitconfig" "${HOME}/.gitconfig"
 
 # Install diff-highlight
 mkdir ${HOME}/bin
-cp /usr/local/share/git-core/contrib/diff-highlight/diff-highlight ~/bin
+cp $(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight ~/bin
