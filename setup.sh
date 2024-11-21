ad784434205f98b972d0b823aa995928191860cd
Yoshihide Jimbo <yjimbo@gmail.com>
2024-11-21 20:37:36 +0900

    Improve the setup script

--- setup.sh
+++ setup.sh
@@ -1,79 +1,108 @@
 #!/usr/bin/env bash
 
+set -eu
+
 DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 
-# Logging stuff
-function log_warn()    { echo -e "\x1B[31m\x1B[4mWarning\x1B[24m\x1B[39m: $@"; }
-function log_error()   { echo -e "\x1B[31m\x1B[4mError\x1B[24m\x1B[39m: $@"; }
-function log_ok()      { echo -e "\x1B[32m==> \x1B[39m$@"; }
-function log_success() { echo -e "\x1B[32m$@\x1B[39m"; }
-function log_fail()    { echo -e "\x1B[31m$@\x1B[39m"; }
-function log_header()  { echo -e "\x1B[4m$@\x1B[24m"; }
-function log_subhead() { echo -e "\x1B[1m$@\x1B[22m"; }
-function log_info()    { echo -e "\x1B[33m==> \x1B[39m$@"; }
+# Logging functions with proper argument handling
+function log_header() { echo -e "\n\x1B[4m$*\x1B[24m"; }
+function log_info() { echo -e "\x1B[33m==> \x1B[39m$*"; }
+function log_warn() { echo -e "\x1B[31m\x1B[4mWarning\x1B[24m\x1B[39m: $*"; }
+function log_error() { echo -e "\x1B[31m\x1B[4mError\x1B[24m\x1B[39m: $*"; }
+function log_ok() { echo -e "\x1B[32m==> \x1B[39m$*"; }
+function log_success() { echo -e "\x1B[32m$*\x1B[39m"; }
+function log_fail() { echo -e "\x1B[31m$*\x1B[39m"; }
 
+# Function to run setup scripts
+run_setup() {
+  local script="$1"
+  if [[ -f $script ]]; then
+    bash "$script"
+  else
+    log_error "Setup script not found: $script"
+  fi
+}
+
+# Check for command
+check_command() {
+  if command -v "$1" > /dev/null 2>&1; then
+    log_ok "$1 is available"
+  else
+    log_error "Command not found: $1"
+    exit 1
+  fi
+}
 # Check dependencies
 # ----------------------------------------------------------------------
 
-# Check for homebrew
-if [[ ! $(type -P brew) ]]; then
-    log_header "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew if not present
+log_header "Installing Homebrew..."
+if ! command -v brew > /dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  log_info "Homebrew is already installed, checking for updates..."
+  brew update
 fi
-
 
 # Preparation
 # ----------------------------------------------------------------------
 
-brew install git
+log_header "Installing git..."
+if ! command -v git > /dev/null 2>&1; then
+  brew install git
+  log_ok "Git installed successfully"
+else
+  log_info "Git is already installed, checking for updates..."
+  brew upgrade git
+fi
 
 # Check that the connection to the github server
-ssh -vT git@github.com >/dev/null 2>&1
-if [ $? -ne 1 ]; then
-    log_fail "git@github.com: Permission denied (publickey)."
-    log_fail "Please generate a SSH key and associate it with GitHub."
-    log_fail "See https://help.github.com/articles/error-permission-denied-publickey/ for more details."
-    exit 1
+log_header "Checking the connection to the github server..."
+if ssh -T git@github.com 2>&1 | grep -q "successfully authenticated"; then
+  log_ok "GitHub connection successful"
+else
+  log_fail "Failed to authenticate with GitHub"
+  log_fail "Please check your SSH key and GitHub account settings"
+  exit 1
 fi
 
 # Clone repo
 # ----------------------------------------------------------------------
 
+log_header "Setting up dotfiles..."
 if [[ ! -d ${DOTFILES_DIRECTORY} ]]; then
-    log_header "Downloading dotfiles..."
-    git clone --recursive git@github.com:jmblog/dotfiles.git ${DOTFILES_DIRECTORY}
-    cd ${DOTFILES_DIRECTORY}
+  git clone --recursive git@github.com:jmblog/dotfiles.git "${DOTFILES_DIRECTORY}"
+  log_ok "Dotfiles repository cloned successfully"
+else
+  git -C "${DOTFILES_DIRECTORY}" pull origin main
+  log_ok "Dotfiles repository updated successfully"
 fi
+
+cd "${DOTFILES_DIRECTORY}"
 
 # Setup git
 # ----------------------------------------------------------------------
 
-log_header "Setting git..."
-bash ./git/setup.sh
+log_header "Setting up git..."
+run_setup "./git/setup.sh"
 
-# Setup zsh
+# Setup git
 # ----------------------------------------------------------------------
 
-log_header "Setting zsh..."
-bash ./zsh/setup.sh
+log_header "Setting up zsh..."
+run_setup "./zsh/setup.sh"
 
 # Setup vim
 # ----------------------------------------------------------------------
 
-log_header "Setting vim..."
-ln -fs "${DOTFILES_DIRECTORY}/vim/vimrc" "${HOME}/.vimrc"
-ln -fs "${DOTFILES_DIRECTORY}/vim" "${HOME}/.vim"
+log_header "Setting up vim..."
+run_setup "./vim/setup.sh"
 
-
-# Setup node
+# Setup Node.js
 # ----------------------------------------------------------------------
 
-log_header "Installing the latest node..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-nvm install node
-nvm use node
-
-bash ./node/npm.sh
+log_header "Setting up Node.js..."
+run_setup "./node/setup.sh"
 
 # Install macOS packages
 # ----------------------------------------------------------------------
@@ -84,18 +113,28 @@ bash ./homebrew/brew.sh
 # Install macOS apps
 # ----------------------------------------------------------------------
 
-#log_header "Installing macOS apps..."
-#bash ./homebrew/cask.sh
-#log_info "Some apps should be installed manually..."
-#bash ./applist.sh
+log_header "Installing macOS apps..."
+read -p "Do you want to install macOS apps using Homebrew Cask? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  bash ./homebrew/cask.sh
+fi
+
+read -p "Do you want to install macOS apps manually? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  bash ./applist.sh
+fi
 
 # Install fonts
 # ----------------------------------------------------------------------
 
 log_header "Installing fonts..."
-bash ./homebrew/font.sh
-log_info "Download San Fransisco font from https://developer.apple.com/fonts/ manually."
-
+read -p "Do you want to install fonts? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  bash ./homebrew/font.sh
+fi
 
 # Setup item2
 # http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
@@ -103,7 +142,7 @@ log_info "Download San Fransisco font from https://developer.apple.com/fonts/ ma
 
 log_header "Setting iterm2..."
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
@@ -113,15 +152,16 @@ defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 # log_header "Setting macOS system defaults..."
 # bash ./macosdefaults.sh
 
-
 # Create some directories
 # ----------------------------------------------------------------------
 
-log_header "Create the regular directories..."
-mkdir ~/Projects
-mkdir ~/Sandbox
+log_header "Creating regular directories..."
+mkdir -p ~/Projects ~/Sandbox
 
 # Cleanup
 # ----------------------------------------------------------------------
 
+log_header "Cleaning up..."
 brew cleanup
+
+log_success "Dotfiles setup completed successfully!"
