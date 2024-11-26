e0854434b86f8b60baff588097d0947cbea16fc9
Yoshihide Jimbo <yjimbo@gmail.com>
2024-11-27 00:31:17 +0900

    Comment out GOOGLE_APPLICATION_CREDENTIALS path in ubie.zsh

--- zsh/ubie.zsh
+++ zsh/ubie.zsh
@@ -2,4 +2,4 @@
 
 export PATH="$HOME/.ubie-bin:$PATH"
 export PATH="$HOME/Projects/ubie-inc/eng-tools:$PATH"
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Projects/ubie-inc/eng-tools/local-sa-key.json"
+# export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Projects/ubie-inc/eng-tools/local-sa-key.json"
