22fcd52b5162cf676f13ab6810236aee7f0eeb1e
Yoshihide Jimbo <yjimbo@gmail.com>
2024-11-27 21:04:32 +0900

    Enhance Alacritty setup script to check for existing directories and update themes

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 DOTFILES_DIRECTORY="${HOME}/.dotfiles"
+ALACRITTY_CONFIG_DIRECTORY="${HOME}/.config/alacritty"
+ALACRITTY_THEMES_DIRECTORY="${ALACRITTY_CONFIG_DIRECTORY}/themes"
 
-mkdir -p ~/.config/alacritty/themes
+if [ ! -d "$ALACRITTY_CONFIG_DIRECTORY/themes" ]; then
+  mkdir -p ~/.config/alacritty/themes
+fi
 
-ln -fs "${DOTFILES_DIRECTORY}/alacritty/alacritty.toml" "${HOME}/.config/alacritty/alacritty.toml"
+ln -fs "${DOTFILES_DIRECTORY}/alacritty/alacritty.toml" "${ALACRITTY_CONFIG_DIRECTORY}/alacritty.toml"
 
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+if [[ ! -d ${ALACRITTY_THEMES_DIRECTORY} ]]; then
+  git clone https://github.com/alacritty/alacritty-theme "${ALACRITTY_THEMES_DIRECTORY}"
+else
+  git -C "${ALACRITTY_THEMES_DIRECTORY}" pull origin master
+fi
 
 # https://github.com/alacritty/alacritty/issues/4673#issuecomment-771291615
 spctl --add /Applications/Alacritty.app
