4f4c49400e345660ade05bb02262641f9ee1b7ab
Yoshihide Jimbo <yjimbo@gmail.com>
2025-02-21 16:15:12 +0900

    Add Git hooks and update Git configuration

--- git/setup.sh
+++ git/setup.sh
@@ -4,11 +4,19 @@ DOTFILES_DIRECTORY="${HOME}/.dotfiles"
 
 # Install GitHub CLI
 brew install gh
-gh completion -s zsh > $(brew --prefix)/share/zsh/site-functions/_gh
+gh completion -s zsh >$(brew --prefix)/share/zsh/site-functions/_gh
 
 ln -fs "${DOTFILES_DIRECTORY}/git/gitattributes" "${HOME}/.gitattributes"
 ln -fs "${DOTFILES_DIRECTORY}/git/gitignore" "${HOME}/.gitignore"
 ln -fs "${DOTFILES_DIRECTORY}/git/gitconfig" "${HOME}/.gitconfig"
+
+# Create .config/git directory if it doesn't exist
+if [ ! -d "${HOME}/.config/git" ]; then
+  mkdir -p "${HOME}/.config/git"
+fi
+
+ln -fs "${DOTFILES_DIRECTORY}/git/hooks" "${HOME}/.config/git/hooks"
+chmod +x "${HOME}/.config/git/hooks/post-merge"
 
 # Install diff-highlight
 if [ ! -d "${HOME}/bin" ]; then
